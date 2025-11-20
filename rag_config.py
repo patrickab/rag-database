4b3a003762f231ced8123d132e31b3bb2d44f9a6
patrickab <patrick.a.bitzer@gmail.com>
2025-11-20 20:11:17 +0100

    feat(rag-db): Generalize design for different clients

---
+++ rag_config.py
@@ -0,0 +1,54 @@
+import os
+
+import polars as pl
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+TIMEOUT = 20
+
+# Define RAG database schema
+class DatabaseKeys:
+    """Keys for the RAG DataFrame"""
+
+    KEY_TITLE = "title"
+    KEY_TXT = "text"
+    KEY_EMBEDDINGS = "embeddings"
+    KEY_SIMILARITIES = "similarities"
+
+RAG_SCHEMA = pl.DataFrame(
+    schema={
+        DatabaseKeys.KEY_TITLE: pl.Utf8,
+        DatabaseKeys.KEY_TXT: pl.Utf8,
+        DatabaseKeys.KEY_EMBEDDINGS: pl.List(pl.Float64),
+    }
+)
+
+MODEL_CONFIG = {
+    "embeddinggemma:300m": {
+        "max_tokens": 2048,
+        "dimensions": 768, # max value, also allows smaller sizes
+        "tokenizer": "google/embeddinggemma-300m" # used to select tokenizer
+    },
+    "text-embedding-3-large": {
+        "max_tokens": 8191,
+        "dimensions": 3072,
+        "tokenizer": "DWDMaiMai/tiktoken_cl100k_base"
+    },
+    "text-embedding-3-small": {
+        "max_tokens": 4096,
+        "dimensions": 1536,
+        "tokenizer": "DWDMaiMai/tiktoken_cl100k_base"
+    },
+    "gemini-embedding-001": {
+        "max_tokens": 2048,
+        "dimensions": 3072,
+        "tokenizer": "DWDMaiMai/tiktoken_cl100k_base" # use OpenAI tokenizer as proxy for splitting chunks
+    }
+}
+
+DEFAULT_EMBEDDING_MODEL = "text-embedding-3-large"
+
+OPENAI_EMBEDDING_MODELS = ["text-embedding-3-large", "text-embedding-3-small"]
+GEMINI_EMBEDDING_MODELS = ["gemini-embedding-001"]
+OLLAMA_EMBEDDING_MODELS = ["embeddinggemma:300m"]
--- rag_database.py
+++ rag_database.py
@@ -1,35 +1,19 @@
-import asyncio
-import os
-import json
-import numpy as np
-import polars as pl
-import tiktoken
-
-from openai import OpenAI
 from dataclasses import dataclass
+import json
 
-API_KEY = os.getenv("OPENAI_API_KEY")
-MODEL_NAME = "text-embedding-3-large"
-MAX_TOKENS = 8192  # Max tokens for the embedding model
-EMBEDDING_DIMENSIONS = 3072
+import numpy as np
+from openai import AsyncOpenAI, OpenAI
+import polars as pl
+from transformers import AutoTokenizer
 
-
-class DataframeKeys:
-    """Keys for the RAG DataFrame"""
-
-    KEY_TITLE = "title"
-    KEY_TXT = "text"
-    KEY_EMBEDDINGS = "embeddings"
-    KEY_SIMILARITIES = "similarities"
-
-
-# Define RAG database schema
-RAG_SCHEMA = pl.DataFrame(
-    schema={
-        DataframeKeys.KEY_TITLE: pl.Utf8,
-        DataframeKeys.KEY_TXT: pl.Utf8,
-        DataframeKeys.KEY_EMBEDDINGS: pl.List(pl.Float64),
-    }
+from rag_config import (
+    DEFAULT_EMBEDDING_MODEL,
+    MODEL_CONFIG,
+    OPENAI_API_KEY,
+    OPENAI_EMBEDDING_MODELS,
+    RAG_SCHEMA,
+    TIMEOUT,
+    DatabaseKeys,
 )
 
 
@@ -53,9 +37,9 @@ class RAGResponse:
         """Convert RAGResponse to a JSON string."""
         return json.dumps(
             {
-                DataframeKeys.KEY_TITLE: self.titles,
-                DataframeKeys.KEY_TXT: self.texts,
-                DataframeKeys.KEY_SIMILARITIES: self.similarities,
+                DatabaseKeys.KEY_TITLE: self.titles,
+                DatabaseKeys.KEY_TXT: self.texts,
+                DatabaseKeys.KEY_SIMILARITIES: self.similarities,
             },
             ensure_ascii=False,
         )
@@ -64,85 +48,109 @@ class RAGResponse:
         """Convert RAGResponse to a Polars DataFrame."""
         return pl.DataFrame(
             {
-                DataframeKeys.KEY_SIMILARITIES: self.similarities,
-                DataframeKeys.KEY_TITLE: self.titles,
-                DataframeKeys.KEY_TXT: self.texts,
+                DatabaseKeys.KEY_SIMILARITIES: self.similarities,
+                DatabaseKeys.KEY_TITLE: self.titles,
+                DatabaseKeys.KEY_TXT: self.texts,
             }
         )
-
 
 class EmbeddingModel:
     """Wrapper for the OpenAI Embedding Model"""
 
-    def __init__(self) -> None:
-        """Initialize the embedding model client and tokenizer."""
-        self.client = OpenAI(
-            api_key=API_KEY,
-        )
-        self.tokenizer = tiktoken.get_encoding(MODEL_NAME)
+    def __init__(self, model:str=DEFAULT_EMBEDDING_MODEL) -> None:
+        """Initialize the async embedding client and tokenizer."""
 
-    def split_text(self, text_to_split: str, max_tokens: int) -> list[str]:
-        """Split text into chunks of a maximum token size."""
-        tokens = self.tokenizer.encode(text_to_split)
-        token_chunks = [
-            tokens[i : i + max_tokens] for i in range(0, len(tokens), max_tokens)
+        self.model = model
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_CONFIG[self.model]["tokenizer"])
+
+        def length_function(text:str) -> int: return len(self.tokenizer.encode(text))
+        self.length_function = length_function
+
+        if self.model in OPENAI_EMBEDDING_MODELS:
+            self.openai_sync_client = OpenAI(api_key=OPENAI_API_KEY)
+            self.openai_async_client = AsyncOpenAI(api_key=OPENAI_API_KEY)
+
+    def chunk_texts(self, texts: list[str]) -> list[list[str]]:
+        """Split chunks (naive general purpose approach - split by max tokens, ignores semantics & overlap)."""
+        chunked_texts = []
+        for text in texts:
+            text_chunks = []
+            tokens = self.tokenizer.encode(text)
+            if len(tokens) <= MODEL_CONFIG[self.model]["max_tokens"]:
+                chunked_texts.append([text])
+            else:
+                for i in range(0, len(tokens), MODEL_CONFIG[self.model]["max_tokens"]):
+                    chunk = tokens[i : i + MODEL_CONFIG[self.model]["max_tokens"]]
+                    chunk_text = self.tokenizer.decode(chunk)
+                    text_chunks.append(chunk_text)
+
+                chunked_texts.append(text_chunks)
+
+        return chunked_texts
+
+    def sync_embed(self, document: str) -> np.ndarray:
+        """Embed a single text."""
+
+        if document == "":
+            return np.zeros(MODEL_CONFIG[self.model]["dimensions"])
+
+        chunked_texts = self.chunk_text([document])
+
+        embeddings_list: list[np.ndarray] = []
+        for chunk in chunked_texts:
+            if self.model in OPENAI_EMBEDDING_MODELS:
+                response = self.openai_sync_client.embeddings.create(
+                    input=chunk,
+                    model=self.model,
+                    dimensions=MODEL_CONFIG[self.model]["dimensions"],
+                    timeout=TIMEOUT,
+                )
+                embedding = np.array(response.data[0].embedding)
+                embeddings_list.append(embedding)
+
+        aggregated_embedding = np.mean(embeddings_list, axis=0)
+
+        return aggregated_embedding
+
+    def embed_batch(self, texts: list[str]) -> np.ndarray:
+        """
+        Embed many texts in one API call.
+        Calculates document-level embeddings. Considers chunking.
+        Chapter-wise embeddings can be considered for pdf's articles etc.
+        """
+        chunked_texts = self.chunk_texts(texts)
+
+        # 1. Flatten Chunks & Store Mapping
+        lengths = [len(chunks) for chunks in chunked_texts]
+        flattend_chunked_texts = [chunk for chunks in chunked_texts for chunk in chunks]
+
+        if self.model in OPENAI_EMBEDDING_MODELS:
+
+            # 2. Batch Embed Flattened Chunks
+            response = self.openai_sync_client.embeddings.create(
+                input=flattend_chunked_texts,
+                model=self.model,
+                dimensions=MODEL_CONFIG[self.model]["dimensions"],
+                timeout=TIMEOUT,
+            )
+
+            embeddings = np.array([d.embedding for d in response.data])
+
+        # 3. Reconstruct Structure
+        embeddings = np.split(embeddings, np.cumsum(lengths)[:-1])
+
+        # 4. Aggregate Embeddings per Text
+        aggregated_embeddings = [
+            np.mean(emb_list, axis=0) for emb_list in embeddings
         ]
-        return [self.tokenizer.decode(chunk) for chunk in token_chunks]
-
-    async def embed(self, text: str) -> np.ndarray:
-        """
-        Asynchronously embed a single text into a dense vector.
-        """
-        original_tokens = self.tokenizer.encode(text)
-
-        if len(original_tokens) > MAX_TOKENS:
-            chunks = self.split_text(text, MAX_TOKENS)
-        else:
-            chunks = [text]
-
-        # Request embeddings for each chunk asynchronously
-        async def get_embedding(chunk):
-            return await self.client.embeddings.create(
-                input=chunk,
-                model=MODEL_NAME,
-                dimensions=EMBEDDING_DIMENSIONS,
-                timeout=60,
-            )
-
-        chunk_embeddings = await asyncio.gather(
-            *[get_embedding(chunk) for chunk in chunks]
-        )
-
-        if len(chunk_embeddings) == 1:
-            final_embedding = np.array(chunk_embeddings[0].data[0].embedding)
-        else:
-            # Weighted average based on token counts
-            chunk_token_counts = [len(self.tokenizer.encode(chunk)) for chunk in chunks]
-            total_tokens = sum(chunk_token_counts)
-            weights = [count / total_tokens for count in chunk_token_counts]
-
-            final_embedding = np.average(
-                [response.data[0].embedding for response in chunk_embeddings],
-                axis=0,
-                weights=weights,
-            )
-
-        return final_embedding
-
-    async def batch_embed(self, texts: list[str]) -> np.ndarray:
-        """
-        Asynchronously embed a list of texts into dense vectors.
-
-        Returns:
-            A 2D numpy array of shape (len(texts), embedding dimension)
-        """
-        tasks = [self.embed(text) for text in texts]
-        all_embeddings = await asyncio.gather(*tasks)
-        return np.array(all_embeddings)
+        return aggregated_embeddings
 
 
 class VectorDB:
-    """A simple in-memory vector database using Polars and NumPy."""
+    """
+    A simple in-memory vector database using Polars and NumPy.
+    Supports initialization with a stored Polars DataFrame to avoid rebuilding the DB. 
+    """
 
     def __init__(self, dataframe: pl.DataFrame) -> None:
         if dataframe.schema == RAG_SCHEMA.schema:
@@ -153,7 +161,7 @@ class VectorDB:
             )
             raise ValueError(error_msg)
 
-    def similarity_search(self, query_vector: np.ndarray, k_queries: int = 20):
+    def similarity_search(self, query_vector: np.ndarray, k_queries: int = 20) -> RAGResponse:
         """
         Search for similar vectors in the database using cosine similarity.
 
@@ -164,7 +172,7 @@ class VectorDB:
         Returns:
             RAGResponse
         """
-        db_vectors = np.stack(self.database[DataframeKeys.KEY_EMBEDDINGS].to_list())
+        db_vectors = np.stack(self.database[DatabaseKeys.KEY_EMBEDDINGS].to_list())
 
         # Compute cosine similarity
         dot_product = np.dot(db_vectors, query_vector)
@@ -177,8 +185,8 @@ class VectorDB:
         df_top_k = self.database[top_indices]
 
         return RAGResponse(
-            titles=df_top_k[DataframeKeys.KEY_TITLE].to_list(),
-            texts=df_top_k[DataframeKeys.KEY_TXT].to_list(),
+            titles=df_top_k[DatabaseKeys.KEY_TITLE].to_list(),
+            texts=df_top_k[DatabaseKeys.KEY_TXT].to_list(),
             similarities=cosine_similarities[top_indices].tolist(),
         )
 
@@ -186,8 +194,8 @@ class VectorDB:
 class RagDatabase:
     """Database for Retrieval Augmented Generation (RAG)"""
 
-    def __init__(self, vector_db: VectorDB):
-        self.embedding_model = EmbeddingModel()
+    def __init__(self, vector_db: VectorDB, model:str=DEFAULT_EMBEDDING_MODEL) -> None:
+        self.embedding_model = EmbeddingModel(model=model)
         self.vector_db = vector_db
 
     async def rag_process_query(self, rag_query: RAGQuery) -> RAGResponse:
